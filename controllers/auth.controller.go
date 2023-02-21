0869b652208a9d7d6cc5ec47d4cb96e0557e4e9b
StreetXHasu <69196228+StreetXHasu@users.noreply.github.com>
2023-02-21 22:06:18 +0300

    Feature/basic auth 1.1.0 (#1)
    * some fix
    
    * basic auth + env + controller
    
    * .ebignore
    
    * version 1.1.0

---
+++ controllers/auth.controller.go
@@ -0,0 +1,80 @@
+package controllers
+
+import (
+	"app-learn-golang/initializers"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+	"strings"
+	"time"
+)
+
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+type SignInInput struct {
+	Email    string `json:"email"  validate:"required"`
+	Password string `json:"password"  validate:"required"`
+}
+
+func SignInUser(c *fiber.Ctx) error {
+	var payload SignInInput
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+	}
+
+	resultEmail := strings.ToLower(payload.Email)
+	fmt.Println("payload.Email", strings.ToLower(payload.Email))
+	userEmail, foundEmail := users[resultEmail]
+	if foundEmail != true {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": "Invalid email or Password"})
+	}
+
+	resultPass := strings.ToLower(payload.Password)
+	fmt.Println("payload.Password", strings.ToLower(payload.Password))
+	if userEmail != resultPass {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": "Invalid email or Password"})
+	}
+
+	config, _ := initializers.LoadConfig(".")
+
+	tokenByte := jwt.New(jwt.SigningMethodHS256)
+
+	now := time.Now().UTC()
+	claims := tokenByte.Claims.(jwt.MapClaims)
+
+	claims["sub"] = &users
+	claims["exp"] = now.Add(config.JwtExpiresIn).Unix()
+	claims["iat"] = now.Unix()
+	claims["nbf"] = now.Unix()
+
+	tokenString, err := tokenByte.SignedString([]byte(config.JwtSecret))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{"status": "fail", "message": fmt.Sprintf("generating JWT Token failed: %v", err)})
+	}
+
+	c.Cookie(&fiber.Cookie{
+		Name:     "token",
+		Value:    tokenString,
+		Path:     "/",
+		MaxAge:   config.JwtMaxAge * 60,
+		Secure:   false,
+		HTTPOnly: true,
+		Domain:   "localhost",
+	})
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "token": tokenString})
+}
+
+func LogoutUser(c *fiber.Ctx) error {
+	expired := time.Now().Add(-time.Hour * 24)
+	c.Cookie(&fiber.Cookie{
+		Name:    "token",
+		Value:   "",
+		Expires: expired,
+	})
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success"})
+}
